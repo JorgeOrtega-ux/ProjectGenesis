2445864b1a840ec3f550130ee9e62d132accaf38
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-10-23 11:24:03 -0600

    Add files via upload

---
+++ ProjectGenesis/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded">
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+    <title>ProjectGenesis</title>
+</head>
+
+<body>
+    <div class="page-wrapper">
+        <div class="main-content">
+            <div class="general-content">
+                <div class="general-content-top">
+                    <div class="header">
+                        <div class="header-left">
+                            <div class="header-item">
+                                <div class="header-button">
+                                    <span class="material-symbols-rounded">menu</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="header-right">
+                            <div class="header-item">
+                                <div class="header-button header-profile"></div>
+                            </div>
+                            
+                        </div>
+                        <div class="module-content module-select">
+                                <div class="menu-content"></div>
+                            </div>
+                    </div>
+                </div>
+                <div class="general-content-bottom">
+                    <div class="module-content module-surface">
+                        <div class="menu-content"></div>
+                    </div>
+                    <div class="general-content-scrolleable"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
