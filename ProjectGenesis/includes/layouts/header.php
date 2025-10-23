aa1bff65a11f5f82681387ddda0fba6ad43a5f28
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-10-23 11:45:27 -0600

    Add files via upload

---
+++ ProjectGenesis/includes/layouts/header.php
@@ -0,0 +1,44 @@
+<div class="header">
+    <div class="header-left">
+        <div class="header-item">
+            <div class="header-button" data-action="toggleModuleSurface">
+                <span class="material-symbols-rounded">menu</span>
+            </div>
+        </div>
+    </div>
+    <div class="header-right">
+        <div class="header-item">
+            <div class="header-button header-profile" data-action="toggleModuleSelect"></div>
+        </div>
+    </div>
+    <div class="module-content module-select disabled" data-module="moduleSelect">
+        <div class="menu-content">
+            <div class="menu-list">
+                <div class="menu-link">
+                    <div class="menu-link-icon">
+                        <span class="material-symbols-rounded">settings</span>
+                    </div>
+                    <div class="menu-link-text">
+                        <span>Configuracion</span>
+                    </div>
+                </div>
+                <div class="menu-link">
+                    <div class="menu-link-icon">
+                        <span class="material-symbols-rounded">help</span>
+                    </div>
+                    <div class="menu-link-text">
+                        <span>Ayuda y comentarios</span>
+                    </div>
+                </div>
+                <div class="menu-link">
+                    <div class="menu-link-icon">
+                        <span class="material-symbols-rounded">logout</span>
+                    </div>
+                    <div class="menu-link-text">
+                        <span>Cerrar sesion</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ ProjectGenesis/includes/modules/module-surface.php
@@ -0,0 +1,14 @@
+<div class="module-content module-surface disabled" data-module="moduleSurface">
+    <div class="menu-content">
+        <div class="menu-list">
+            <div class="menu-link">
+                <div class="menu-link-icon">
+                    <span class="material-symbols-rounded">home</span>
+                </div>
+                <div class="menu-link-text">
+                    <span>Pagina principal</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
